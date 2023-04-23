4f26d43834b5f513bb7dea449e539f205b082987
Cristian Mendoza <cfmendoza.audit@gmail.com>
2023-04-22 19:27:21 -0500

    preparacion de formularios

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// forma sencilla
+// const $form = document.querySelector('#form')
+// const $buttonMailto = document.querySelector('#trucazo')
+
+// $form.addEventListener('submit', (event) => {
+//     event.preventDefault()
+//     const formdata = new FormData(event.currentTarget)
+//     $buttonMailto.setAttribute('href', `mailto:servidorfiel42@gmail.com?subject= ${formdata.get('name')} ${formdata.get('lasname')} ${formdata.get('email')} &body= ${formdata.get('message')}`)
+//     $buttonMailto.click()
+//     $form.reset()
+// })
+
+
+
+
+
+
 
 
 
