9f23c3447c5afc7c5a518221e29c73bc6e5fad74
Cristian Mendoza <cfmendoza.audit@gmail.com>
2023-04-27 10:16:09 -0500

    adicion de formularios y enlaces a W

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-// forma sencilla
-// const $form = document.querySelector('#form')
-// const $buttonMailto = document.querySelector('#trucazo')
+// formulario principla
+const $form = document.querySelector('#form')
+const $buttonMailtoP = document.querySelector('#formPrincipal')
 
-// $form.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     const formdata = new FormData(event.currentTarget)
-//     $buttonMailto.setAttribute('href', `mailto:servidorfiel42@gmail.com?subject= ${formdata.get('name')} ${formdata.get('lasname')} ${formdata.get('email')} &body= ${formdata.get('message')}`)
-//     $buttonMailto.click()
-//     $form.reset()
-// })
+$form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formdata = new FormData(event.currentTarget)
+    $buttonMailtoP.setAttribute('href', `mailto:servidorfiel42@gmail.com?subject= ${formdata.get('name')} ${formdata.get('lasname')} ${formdata.get('email')} &body= ${formdata.get('message')}`)
+    $buttonMailtoP.click()
+    $form.reset()
+})
+
 
 
 
